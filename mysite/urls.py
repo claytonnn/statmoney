9e3a19900d26854cd6649e25881614cb02efaa06
Clayton <clayton_rocha@hotmail.com>
2019-01-25 13:34:20 -0200

    pqp6

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lista/', include('statmoney.urls')),
+    path('', include('statmoney.urls')),
 ]
